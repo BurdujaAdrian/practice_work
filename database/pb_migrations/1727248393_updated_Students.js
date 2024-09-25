4f05b3131aa2d13710a342782d4c93a5b138dbc6
arturtugui <146761295+arturtugui@users.noreply.github.com>
2024-09-25 10:37:05 +0300

    .

---
+++ database/pb_migrations/1727248393_updated_Students.js
@@ -0,0 +1,31 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("4i53pyqjukl7lxi")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ernqgvs0",
+    "name": "group_name",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("4i53pyqjukl7lxi")
+
+  // remove
+  collection.schema.removeField("ernqgvs0")
+
+  return dao.saveCollection(collection)
+})
