a7aa90bdc20575e8f1eb030fed5ee50ade668139
Dackohn <145903356+Dackohn@users.noreply.github.com>
2024-09-26 19:26:48 +0300

    Added terminal conf in back

--- server-app/face_recognition/main.py
+++ server-app/face_recognition/main.py
@@ -2,12 +2,12 @@ import cv2
 import numpy as np
 import torch
 from PIL import Image
-import matplotlib.pyplot as plt
 from facenet_pytorch import InceptionResnetV1
 from torchvision.transforms.functional import to_tensor
 from sklearn.metrics.pairwise import cosine_similarity
 import requests
 import json
+import argparse
 
 model = InceptionResnetV1(pretrained='vggface2').eval()
 
@@ -15,10 +15,12 @@ sr = cv2.dnn_superres.DnnSuperResImpl_create()
 sr.readModel("model-weights/ESPCN_x4.pb")
 sr.setModel("espcn", 4)
 
+
 def apply_log_filter(image):
     image_log = np.log1p(np.array(image, dtype="float32"))
     cv2.normalize(image_log, image_log, 0, 255, cv2.NORM_MINMAX)
     return np.uint8(image_log)
+
 
 def preprocess_face_image(face_image):
     face_sr = sr.upsample(face_image)
@@ -29,15 +31,18 @@ def preprocess_face_image(face_image):
     face_tensor = to_tensor(face_pil).unsqueeze(0)
     return face_tensor
 
+
 def extract_face_embedding(face_image):
     face_tensor = preprocess_face_image(face_image)
     with torch.no_grad():
         embedding = model(face_tensor).numpy()
     return embedding
 
+
 def load_yolo_model(config_path, weights_path):
     net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
     return net
+
 
 def detect_faces_yolo(image, net, confidence_threshold=0.3, nms_threshold=0.0):
     blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
@@ -72,6 +77,7 @@ def detect_faces_yolo(image, net, confidence_threshold=0.3, nms_threshold=0.0):
             final_boxes = [boxes[i] for i in indices]
     return final_boxes
 
+
 def admin_login(base_url, admin_email, admin_password):
     url = f"{base_url}/api/admins/auth-with-password"
     data = {"identity": admin_email, "password": admin_password}
@@ -80,26 +86,35 @@ def admin_login(base_url, admin_email, admin_password):
         raise Exception(f"Failed to authenticate admin: {response.text}")
     return response.json()["token"]
 
-def load_people_from_pocketbase(base_url, collection_name, token):
-    url = f"{base_url}/api/collections/{collection_name}/records"
+
+def load_people_from_pocketbase(base_url, collection_name, token, group_list):
+    # Join group_list elements into a comma-separated string to send in the filter
+    group_filter = " || ".join([f'Group="{group}"' for group in group_list])
+
+    # Create the URL with the filter query
+    url = f"{base_url}/api/collections/{collection_name}/records?filter={group_filter}"
     headers = {"Authorization": f"Bearer {token}"}
+
     response = requests.get(url, headers=headers)
+
     if response.status_code != 200:
         raise Exception(f"Failed to fetch records: {response.text}")
+
     data = response.json()["items"]
     people = []
+
     for person in data:
         person_id = person['id']
         person_name = person['name']
         embedding = None
-        print(person['Photo'])
         url2 = f"../../database/pb_data/storage/4i53pyqjukl7lxi/{person_id}/{person['Photo']}"
-        print("Url: ", url2)
         image = cv2.imread(url2)
         if 'Embedding' in person and person['Embedding']:
             embedding = np.array(json.loads(person['Embedding']), dtype=np.float32)
         people.append({'id': person_id, 'name': person_name, 'Embedding': embedding, 'Photo': image})
+
     return people
+
 
 def save_embeddings_to_pocketbase(base_url, collection_name, person_id, embedding, token):
     url = f"{base_url}/api/collections/{collection_name}/records/{person_id}"
@@ -109,6 +124,7 @@ def save_embeddings_to_pocketbase(base_url, collection_name, person_id, embeddin
     response = requests.patch(url, json=data, headers=headers)
     if response.status_code != 200:
         raise Exception(f"Failed to save embedding: {response.text}")
+
 
 def generate_and_save_embeddings(base_url, collection_name, people, net, token):
     for person in people:
@@ -120,6 +136,7 @@ def generate_and_save_embeddings(base_url, collection_name, people, net, token):
             embedding = extract_face_embedding(cropped_face)
             save_embeddings_to_pocketbase(base_url, collection_name, person['id'], embedding, token)
             person['embedding'] = embedding
+
 
 def find_most_similar_face_for_person(image, net, people, similarity_threshold=0.3):
     faces = detect_faces_yolo(image, net)  # Detect all faces in the image
@@ -140,11 +157,10 @@ def find_most_similar_face_for_person(image, net, people, similarity_threshold=0
     return person_matches
 
 
-def match_faces_in_crowd(base_url, collection_name, crowd_image_path, yolo_cfg, yolo_weights, admin_email,
-                         admin_password):
+def match_faces_in_crowd(base_url, collection_name, crowd_image_path, yolo_cfg, yolo_weights, admin_email, admin_password, group_list):
     token = admin_login(base_url, admin_email, admin_password)
     net = load_yolo_model(yolo_cfg, yolo_weights)
-    people = load_people_from_pocketbase(base_url, collection_name, token)
+    people = load_people_from_pocketbase(base_url, collection_name, token, group_list)
     generate_and_save_embeddings(base_url, collection_name, people, net, token)
 
     crowd_image = cv2.imread(crowd_image_path)
@@ -164,17 +180,23 @@ def match_faces_in_crowd(base_url, collection_name, crowd_image_path, yolo_cfg, 
         cv2.rectangle(crowd_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
         cv2.putText(crowd_image, f'{person_name}: {similarity:.2f}%', (x, y - 10),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
     # Print the dictionary
     print(similarity_dict)
     cv2.imwrite("matched_image_sr_log_pocketbase.jpg", crowd_image)
 
 
-base_url = 'http://127.0.0.1:8090'
-collection_name = 'Students'
-crowd_image_path = 'test_group6.jpg'
-yolo_cfg = 'yolov3-face.cfg'
-yolo_weights = 'model-weights/yolov3-wider_16000.weights'
-admin_email = 'admin@gmail.com'
-admin_password = '1234567890'
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Face recognition in crowd images with group filtering")
+    parser.add_argument("crowd_image_path", help="Path to the crowd image")
+    parser.add_argument("group_list", nargs='+', help="List of student groups to filter")
+    args = parser.parse_args()
 
-match_faces_in_crowd(base_url, collection_name, crowd_image_path, yolo_cfg, yolo_weights, admin_email, admin_password)
+    base_url = 'http://127.0.0.1:8090'
+    collection_name = 'Students'
+    yolo_cfg = 'yolov3-face.cfg'
+    yolo_weights = 'model-weights/yolov3-wider_16000.weights'
+    admin_email = 'admin@gmail.com'
+    admin_password = '1234567890'
+
+    match_faces_in_crowd(base_url, collection_name, args.crowd_image_path, yolo_cfg, yolo_weights, admin_email, admin_password, args.group_list)
