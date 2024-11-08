377a31ea2a30695c200626a4da715e876625330e
Dackohn <145903356+Dackohn@users.noreply.github.com>
2024-11-08 19:37:20 +0200

    Added error handling messages to the face recognition program

--- server-app/face_recognition/sandbox2.py
+++ server-app/face_recognition/sandbox2.py
@@ -1,114 +1,191 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+import torch
+from PIL import Image
+from facenet_pytorch import InceptionResnetV1, MTCNN
+from torchvision.transforms.functional import to_tensor
+from sklearn.metrics.pairwise import cosine_similarity
+import requests
+import json
+import argparse
 import os
-import time
 
-i = 0  # Global variable to track the image index
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Create a new directory to save grouped face images
-os.makedirs('output_faces/grouped_faces', exist_ok=True)
+# Load models
+model = InceptionResnetV1(pretrained='vggface2').eval()
+mtcnn = MTCNN(keep_all=True, post_process=False)
 
-# Load super-resolution model
+# Initialize super-resolution model
 sr = cv2.dnn_superres.DnnSuperResImpl_create()
-sr.readModel("model-weights/ESPCN_x4.pb")
-sr.setModel("espcn", 4)  # Set the model to upscale by a factor of 4
+sr.readModel(os.path.join(script_dir, "model-weights/ESPCN_x4.pb"))
+sr.setModel("espcn", 4)
 
-# Logarithmic filter for contrast enhancement
 def apply_log_filter(image):
     image_log = np.log1p(np.array(image, dtype="float32"))
     cv2.normalize(image_log, image_log, 0, 255, cv2.NORM_MINMAX)
     return np.uint8(image_log)
 
-# YOLO face detection
-def load_yolo_model(config_path, weights_path):
-    net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
-    return net
+def preprocess_face_image(face_image):
+    if face_image is None or face_image.size == 0:
+        raise ValueError("Received an empty face image for preprocessing.")
+    face_sr = sr.upsample(face_image)
+    face_log = apply_log_filter(face_sr)
+    face_resized = cv2.resize(face_log, (160, 160))
+    face_rgb = cv2.cvtColor(face_resized, cv2.COLOR_BGR2RGB)
+    face_pil = Image.fromarray(face_rgb)
+    face_tensor = to_tensor(face_pil).unsqueeze(0)
+    return face_tensor
 
-def detect_faces_yolo(image, net, confidence_threshold=0.3, nms_threshold=0.0):
-    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
-    net.setInput(blob)
-    layer_names = net.getLayerNames()
+def extract_face_embedding(face_image):
+    face_tensor = preprocess_face_image(face_image)
+    with torch.no_grad():
+        embedding = model(face_tensor).numpy()
+    return embedding
 
-    # Handle both cases for getUnconnectedOutLayers()
-    unconnected_layers = net.getUnconnectedOutLayers()
-
-    if isinstance(unconnected_layers, np.ndarray):
-        output_layers = [layer_names[i - 1] for i in unconnected_layers.flatten()]
+def detect_faces_mtcnn(image):
+    image_pil = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    boxes, _ = mtcnn.detect(image_pil)
+    if boxes is not None:
+        final_boxes = boxes.astype(int).tolist()
+        visualize_faces(image,final_boxes)
+        for (x1, y1, x2, y2) in final_boxes:
+            print(f"Detected face at: (x1={x1}, y1={y1}, x2={x2}, y2={y2}), width={x2-x1}, height={y2-y1}")
+        return final_boxes
     else:
-        output_layers = [layer_names[unconnected_layers - 1]]
+        print("No faces detected.")
+        return []
 
-    layer_outputs = net.forward(output_layers)
-    height, width = image.shape[:2]
-    boxes = []
-    confidences = []
+def visualize_faces(image, boxes):
+    for (x1, y1, x2, y2) in boxes:
+        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    cv2.imshow("Detected Faces", image)
+    cv2.waitKey(0)  # Press any key to close the window
+    cv2.destroyAllWindows()
 
-    for output in layer_outputs:
-        for detection in output:
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
-            if confidence > confidence_threshold:
-                box = detection[0:4] * np.array([width, height, width, height])
-                (centerX, centerY, w, h) = box.astype("int")
-                x = int(centerX - (w / 2))
-                y = int(centerY - (h / 2))
-                boxes.append([x, y, int(w), int(h)])
-                confidences.append(float(confidence))
 
-    # Apply non-maximum suppression to remove overlapping boxes
-    indices = cv2.dnn.NMSBoxes(boxes, confidences, confidence_threshold, nms_threshold)
 
-    # Ensure indices are correctly handled
-    final_boxes = []
-    if len(indices) > 0:
-        if isinstance(indices[0], (list, np.ndarray)):
-            final_boxes = [boxes[i[0]] for i in indices]  # List of lists case
-        else:
-            final_boxes = [boxes[i] for i in indices]  # Flat list case
+def admin_login(base_url, admin_email, admin_password):
+    url = f"{base_url}/api/admins/auth-with-password"
+    data = {"identity": admin_email, "password": admin_password}
+    response = requests.post(url, json=data)
+    if response.status_code != 200:
+        raise Exception(f"Failed to authenticate admin: {response.text}")
+    return response.json()["token"]
 
-    return final_boxes
+def load_people_from_pocketbase(base_url, collection_name, token, group_list):
+    group_filter = " || ".join([f'Group="{group}"' for group in group_list])
+    url = f"{base_url}/api/collections/{collection_name}/records?filter={group_filter}"
+    headers = {"Authorization": f"Bearer {token}"}
 
-# Save detected faces with and without enhancements
-def save_faces(image, boxes):
-    global i  # Declare i as a global variable to modify it
+    response = requests.get(url, headers=headers)
 
-    for (x, y, w, h) in boxes:
-        face_image = image[y:y + h, x:x + w]
+    if response.status_code != 200:
+        raise Exception(f"Failed to fetch records: {response.text}")
 
-        # Create a sub-folder for each face group
-        folder_path = f'output_faces/grouped_faces/face_group_{i}'
-        os.makedirs(folder_path, exist_ok=True)
+    data = response.json()["items"]
+    people = []
 
-        # Save original face
-        cv2.imwrite(f"{folder_path}/face_{i}_original.jpg", face_image)
+    for person in data:
+        person_id = person['id']
+        person_name = person['name']
+        embedding = None
+        url2 = f"pb_data/storage/4i53pyqjukl7lxi/{person_id}/{person['Photo']}"
+        image = cv2.imread(url2)
+        if 'Embedding' in person and person['Embedding']:
+            embedding = np.array(json.loads(person['Embedding']), dtype=np.float32)
+        people.append({'id': person_id, 'name': person_name, 'embedding': embedding, 'Photo': image})
 
-        # Apply super-resolution
-        face_sr = sr.upsample(face_image)
-        cv2.imwrite(f"{folder_path}/face_{i}_super_resolution.jpg", face_sr)
+    return people
 
-        # Apply logarithmic filter
-        face_log = apply_log_filter(face_sr)
-        cv2.imwrite(f"{folder_path}/face_{i}_log_filtered.jpg", face_log)
+def save_embeddings_to_pocketbase(base_url, collection_name, person_id, embedding, token):
+    url = f"{base_url}/api/collections/{collection_name}/records/{person_id}"
+    embedding_str = json.dumps(embedding.tolist())
+    data = {'Embedding': embedding_str}
+    headers = {'Authorization': f'Bearer {token}', 'Content-Type': 'application/json'}
+    response = requests.patch(url, json=data, headers=headers)
+    if response.status_code != 200:
+        raise Exception(f"Failed to save embedding: {response.text}")
 
-        i += 1
+def generate_and_save_embeddings(base_url, collection_name, people, token):
+    for person in people:
+        if person['embedding'] is None:
+            image = person["Photo"]
+            faces = detect_faces_mtcnn(image)
+            if faces:
+                (x, y, w, h) = faces[0]  # Take the first detected face
+                cropped_face = image[y:y + h, x:x + w]
+                embedding = extract_face_embedding(cropped_face)
+                #save_embeddings_to_pocketbase(base_url, collection_name, person['id'], embedding, token)
+                person['embedding'] = embedding
 
-# Load the crowd image
-crowd_image = cv2.imread('test_group1.jpg')
-net = load_yolo_model("model-weights/yolov3-face.cfg", "model-weights/yolov3-wider_16000.weights")
 
-# Detect faces in the crowd image
-faces = detect_faces_yolo(crowd_image, net)
+def find_most_similar_face_for_person(image, people, similarity_threshold=0.3):
+    faces = detect_faces_mtcnn(image)
+    if not faces:
+        print("No faces detected in the crowd image.")
+        return []
 
-# Save detected faces
-save_faces(crowd_image, faces)
+    person_matches = []
+    for person in people:
+        best_similarity, best_match = -1, None
+        for (x, y, w, h) in faces:
+            if w <= 0 or h <= 0:
+                continue  # Skip invalid face regions
 
-# Draw bounding boxes around detected faces and display the final image
-for (x, y, w, h) in faces:
-    cv2.rectangle(crowd_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            face_image = image[y:y + h, x:x + w]
+            if face_image.size == 0:
+                print("Skipping empty face image.")
+                continue
 
-plt.imshow(cv2.cvtColor(crowd_image, cv2.COLOR_BGR2RGB))
-plt.axis('off')
-plt.show()
+            try:
+                embedding = extract_face_embedding(face_image)
+            except ValueError as e:
+                print(f"Error processing face image: {e}")
+                continue
 
-cv2.imwrite("output_faces/detected_faces.jpg", crowd_image)
+            similarity = cosine_similarity(embedding, person['embedding'].reshape(1, -1))[0][0]
+            if similarity > best_similarity and similarity > similarity_threshold:
+                best_similarity = similarity
+                best_match = {'person': person, 'box': (x, y, w, h), 'similarity': similarity}
+
+        if best_match:
+            person_matches.append(best_match)
+
+    return person_matches
+
+
+def match_faces_in_crowd(base_url, collection_name, crowd_image_path, admin_email, admin_password, group_list):
+    token = admin_login(base_url, admin_email, admin_password)
+    people = load_people_from_pocketbase(base_url, collection_name, token, group_list)
+    generate_and_save_embeddings(base_url, collection_name, people, token)
+
+    crowd_image = cv2.imread(crowd_image_path)
+    matches = find_most_similar_face_for_person(crowd_image, people)
+
+    similarity_dict = {}
+
+    for match in matches:
+        (x, y, w, h) = match['box']
+        person_name = match['person']['name']
+        similarity = match['similarity'] * 100
+        similarity_dict[person_name] = f'{similarity:.2f}%'
+        cv2.rectangle(crowd_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(crowd_image, f'{person_name}: {similarity:.2f}%', (x, y - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+    print(similarity_dict)
+    cv2.imwrite("matched_image_sr_log_pocketbase.jpg", crowd_image)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Face recognition in crowd images with group filtering")
+    parser.add_argument("crowd_image_path", help="Path to the crowd image")
+    parser.add_argument("group_list", nargs='+', help="List of student groups to filter")
+    args = parser.parse_args()
+
+    base_url = 'http://127.0.0.1:8090'
+    collection_name = 'Students'
+    admin_email = 'admin@gmail.com'
+    admin_password = '1234567890'
+
+    match_faces_in_crowd(base_url, collection_name, args.crowd_image_path, admin_email, admin_password, args.group_list)
